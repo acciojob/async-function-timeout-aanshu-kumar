e5c293b9d17128ea68aa3c7ea28e182c88b6ece9
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-03 14:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+let text = document.getElementById("text");
+let number = document.getElementById("delay");
+
+
+let promiseOne =()=>{ 
+
+    return new Promise((resolve , reject)=>{
+    setTimeout(()=>{
+        resolve (text.value);
+    },Number(number.value))
+})
+}
+
+
+btn.addEventListener("click", async () => {
+let data;
+try{
+    data = await promiseOne();
+}
+catch(e){
+    console.log(e);
+}
+   
+    output.innerHTML = data;
+
+});
